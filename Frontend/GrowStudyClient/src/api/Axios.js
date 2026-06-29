559b1dba645b38d8fd8138d3323fb7fec790acb1
Suman Kumar Sahu <sumankumarsahu221@gmail.com>
2026-06-29 17:26:09 +0530

    initialize axios instance with base URL

--- Frontend/GrowStudyClient/src/api/Axios.js
+++ Frontend/GrowStudyClient/src/api/Axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-import dotenv from 'dotenv'
-dotenv.config({ path: '../.env' })
 
 const api = axios.create({
-  baseURL: process.env.BACKENDURL || "http://localhost:3000/api",
+  baseURL: "https://growstudy-backend.onrender.com/api" || "http://localhost:3000/api",
   withCredentials: true,
 });
 
