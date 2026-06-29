43d5d9c4219d24eda8ab0ddb189ef6546455d96c
Suman Kumar Sahu <sumankumarsahu221@gmail.com>
2026-06-29 17:24:15 +0530

    initialize axios instance with environment-based base URL configuration

--- Frontend/GrowStudyClient/src/api/Axios.js
+++ Frontend/GrowStudyClient/src/api/Axios.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import dotenv from 'dotenv'
+dotenv.config({ path: '../.env' })
 
 const api = axios.create({
   baseURL: process.env.BACKENDURL || "http://localhost:3000/api",
