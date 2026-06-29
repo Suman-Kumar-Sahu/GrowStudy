30194e60db0f604e0ddca173172ab4c1abd30300
Suman Kumar Sahu <sumankumarsahu221@gmail.com>
2026-06-29 22:04:25 +0530

    cloudinary upload issue

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv"
 import { app } from './app.js'
 import connectDB from "./src/db/db.js"
+import fs from "fs";
 
 dotenv.config({
     path: "./.env"
 })
+
+const dirs = ["./public/temp", "./public/resume"];
+dirs.forEach(dir => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 
